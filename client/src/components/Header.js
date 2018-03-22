b780fbe6329045fa3c66c13ca94440d872682028
jb <jasontbrown99@gmail.com>
2018-03-22 16:18:40 -0700

    add header and search components

---
+++ client/src/components/Header.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Search from './Search';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
+
+NavbarBrand.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+  // pass in custom element to use
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">Food Finder</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <Search />
+              <NavItem>
+                <NavLink href="#">Continue as guest</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Sign Up</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Login</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  DropDown Menu
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default Header;
---
+++ client/src/components/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Input } from 'reactstrap';
+
+Input.propTypes = {
+  children: PropTypes.node,
+  // type can be things like text, password, (typical input types) as well as select and textarea, providing children as you normally would to those.
+  type: PropTypes.string,
+  size: PropTypes.string,
+  bsSize: PropTypes.string,
+  // state: deprecated(PropTypes.string, 'Please use the prop "valid"'),
+  valid: PropTypes.bool, // applied the is-valid class when true, does nothing when false
+  invalid: PropTypes.bool, // applied the is-invalid class when true, does nothing when false
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  // ref will only get you a reference to the Input component, use innerRef to get a reference to the DOM input (for things like focus management).
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  // static: deprecated(PropTypes.bool, 'Please use the prop "plaintext"'),
+  plaintext: PropTypes.bool,
+  addon: PropTypes.bool,
+  className: PropTypes.string,
+  cssModule: PropTypes.object,
+};
+
+export default class Search extends Component {
+  render() {
+    return (
+      <form className='navbar-form navbar-left' action="">
+        <Input type='text' placeholder='coffee, dinner, bars ...' />{' '}
+        <Input type='text' placeholder='location' />{' '}
+        <Button bsStyle='success' type='submit'>Search</Button>
+      </form>
+    );
+  }
+}
